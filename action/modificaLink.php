798c71d9ab200d041aede946140a5b0e2ce07d2d
alexnevess <alexsndsf@gmail.com>
2025-07-31 12:21:57 -0300

    Finaliza modificaLink

--- action/modificaLink.php
+++ action/modificaLink.php
@@ -4,9 +4,7 @@ include "../config/conecta.php";
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $link = $_POST['link'] ?? "";
 
-    // $teste = stristr($link, "http://");
-    // var_dump($teste);
-
+    //Adiciona htpps ao link 
     if (stristr($link, "http://") !== false) {
         $link = str_replace("http://", "", $link);
         $link = "https://" . $link;
@@ -14,11 +12,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $link = "https://" . $link;
     }
 
-    echo $link;
+    do {
+        $cod = mt_rand(10000, 999999); //gera código
+        $cod = dechex($cod); //transforma o código em hexadecimal
 
-    $cod = mt_rand(10000, 999999);
-    $cod = dechex($cod);
+        //testa se o código já existe no bd
+        $sqlConsulta = "SELECT mylink FROM mylink WHERE mylink = ?";
+        $executa = $con->prepare($sqlConsulta);
+        $executa->bind_param("s", $cod);
+        $executa->execute();
+        $resultado = $executa->get_result();
+        $linhas = $resultado->fetch_assoc();
+    } while ($linhas !== null);//Se for diferente de null gera um novo código
 
+    //Salva o link original modificado e o novo link no BD
     $sql = "INSERT INTO mylink (link, mylink) VALUES (?,?)";
     $consulta = $con->prepare($sql);
     $consulta->bind_Param("ss", $link, $cod);
